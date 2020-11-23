3f9a01d5be0ad70b57d8b1d4b6c0158ae9581d81
CM Rutz <catrutz@pdx.edu>
2020-11-22 20:16:27 -0800

    fixed server to match game needs better

--- graveyard/networking/src/server.rkt
+++ graveyard/networking/src/server.rkt
@@ -13,108 +13,108 @@
 
 ; HISTORY
 
-(define (credentials-ok? game-data game-name game-pwd)
+(define (destroy-game game-data game-name)
+  (hash-remove game-data game-name))
+
+(define (set-update-time game-data id name)
+  (define game (hash-ref game-data name))
+  (hash-set game-data name (hash-set game (string-join (list id "-lastl-updated") "") (current-seconds))))
+
+(define (op-keepalive game-data id name pwd resp-msg)
+  (resp-msg (string-join (list "A" id name pwd) ":")) ; op-keepalive
+  (set-update-time game-data id name))
+
+(define (op-update? game-data id name pwd resp-msg c-move-from c-move-to)
+  (define s-last-move (hash-ref (hash-ref game-data name) "last-move")) ;stored as (list # #)
+  (define c-last-move (list c-move-from c-move-to))
+  (if (not (equal? s-last-move c-last-move))
+      (resp-msg (string-join (list "D" id name pwd (number->string (car s-last-move)) (number->string (last s-last-move))) ":")) ; op-forward-update
+      (resp-msg (string-join (list "A" id name pwd) ":"))) ; op-keepalive
+  (set-update-time game-data id name))
+
+(define (op-update game-data id name pwd resp-msg move-from move-to)
+  (define game-to-update (hash-ref game-data name))
+  (resp-msg (string-join (list "D" id name pwd (number->string move-from) (number->string move-to)) ":")) ; op-forward-update
+  (set-update-time (hash-set game-data name (hash-set game-to-update "last-move" (list move-from move-to))) id name))
+
+(define (op-create game-data id name pwd pieces piece-to-player player-going-first resp-msg)
+  (println "creating a game...")
+  (println game-data)
+  (println name)
+  (cond
+    [(hash-has-key? game-data name) (begin (resp-msg "Q") game-data)] ; err-invalid-name (name already exists)
+    [(>= (hash-count game-data) 5) (begin (resp-msg "T") game-data)] ; err-too-many-games
+    [else (begin (resp-msg (string-join (list "F" id name pwd) ":")) ; op-game-created
+                 (hash-set game-data name (hash "pieces" pieces
+                                                "piece-to-player" piece-to-player
+                                                "player-going-first" player-going-first
+                                                "last-move" (list 84 84)
+                                                "pwd" pwd
+                                                "1-connected" #t
+                                                "2-connected" null
+                                                "1-last-updated" (current-seconds)
+                                                "2-last-updated" null)))]))
+
+(define (op-join game-data id name pwd resp-msg)
+  (if (null? (hash-ref (hash-ref game-data name) "2-connected"))
+      (let ([piece-to-player (hash-ref (hash-ref game-data name) "piece-to-player")]
+            [pieces (hash-ref (hash-ref game-data name) "pieces")]
+            [going-first (number->string (hash-ref (hash-ref game-data name) "player-going-first"))])
+        (resp-msg (string-join (list "H" id name pwd pieces piece-to-player going-first) ":")) ; op-forward-join
+        (hash-set game-data name (hash-set* (hash-ref game-data name) "2-connected" #t "2-last-updated" (current-seconds))))
+      (begin (resp-msg "R") ; err-3s-a-crowd
+             game-data)))
+
+(define (op-joined? game-data id name pwd resp-msg)
+  (define is-2-connected? (hash-ref (hash-ref game-data name) "2-connected"))
+  (cond
+    [(null? is-2-connected?) (resp-msg (string-join (list "A" id name pwd) ":"))] ; op-keepalive
+    [(equal? #t is-2-connected?) (let ([piece-to-player (hash-ref (hash-ref game-data name) "piece-to-player")]
+                                       [pieces (hash-ref (hash-ref game-data name) "pieces")]
+                                       [going-first (number->string (hash-ref (hash-ref game-data name) "player-going-first"))])
+                                   (resp-msg (string-join (list "H" id name pwd pieces piece-to-player going-first) ":")))] ; op-forward-join
+    [(not is-2-connected?) (resp-msg (string-join (list "J" id name pwd) ":"))]) ; op-forward-leave
+  (set-update-time game-data id name))
+
+(define (credentials-ok? game-data name pwd)
   (println "credentials-ok?")
   (println game-data)
-  (println game-name)
-  (println game-pwd)
-  (define stuff (and (hash-has-key? game-data game-name) (equal? (hash-ref (hash-ref game-data game-name) "pwd") game-pwd)))
+  (println name)
+  (println pwd)
+  (define stuff (and (hash-has-key? game-data name) (equal? (hash-ref (hash-ref game-data name) "pwd") pwd)))
   (println stuff)
   stuff)
 
-(define (set-update-time game-data client-id game-name)
-  (define game (hash-ref game-data game-name))
-  (hash-set game-data game-name (hash-set game (string-join (list client-id "-lastl-updated") "") (current-seconds))))
+(define (exec-opcode game-data code id name pwd msg1 msg2 msg3 resp-msg)
+  (println "structure correct!")
+  (if (or (equal? code "E") (credentials-ok? game-data name pwd)) ; if game is being created, credentials won't be stored yet
+      (cond
+        [(equal? code "A") (op-keepalive game-data id name pwd resp-msg)]
+        [(equal? code "B") (op-update? game-data id name pwd resp-msg (string->number msg1) (string->number msg2))] 
+        [(equal? code "C") (op-update game-data id name pwd resp-msg (string->number msg1) (string->number msg2))] 
+        [(equal? code "E") (op-create game-data id name pwd msg1 msg2 (string->number msg3) resp-msg)] 
+        [(equal? code "G") (op-join game-data id name pwd resp-msg)] 
+        [(equal? code "K") (op-joined? game-data id name pwd resp-msg)] 
 
-(define (destroy-game game-data game-name)
-  ; FIX:
-  ; (kill-thread (hash-ref (hash-ref game-data game-name) "controller"))
-  (hash-remove game-data game-name))
-
-(define (op-keepalive game-data client-id game-name game-pwd response-template)
-  (response-template (string-join (list "A" client-id game-name game-pwd ",") ":")) ; op-keepalive
-  (set-update-time game-data client-id game-name))
-
-(define (op-update? game-data client-id game-name game-pwd response-template client-move-from client-move-to)
-  (define server-last-move (hash-ref (hash-ref game-data game-name) "last-move")) ;stored as (list # #)
-  (define client-last-move (list client-move-from client-move-to))
-  (if (not (equal? server-last-move client-last-move))
-      (response-template (string-join (list "D" 
-                                        client-id 
-                                        game-name 
-                                        game-pwd 
-                                        (string-join (list (number->string (car server-last-move)) 
-                                                       "," 
-                                                       (number->string (last server-last-move))) "")) ":")) ; op-forward-update
-      (response-template (string-join (list "A" client-id game-name game-pwd ",") ":"))) ; op-keepalive
-  (set-update-time game-data client-id game-name))
-
-(define (op-update game-data client-id game-name game-pwd response-template move-from move-to)
-  (define game-to-update (hash-ref game-data game-name))
-  (define game-controller (hash-ref game-to-update "controller"))
-  ; FIX: update controller with new move
-  (if #t ; FIX: if controller updated successfully
-      (begin (response-template (string-join (list "D" 
-                                               client-id 
-                                               game-name 
-                                               game-pwd 
-                                               (string-join (list (number->string move-from) 
-                                                              "," 
-                                                              (number->string move-to)) ""))  ":")) ; op-forward-update
-             (hash-set game-data game-name (hash-set game-to-update "last-move" (list move-from move-to))))
-      (begin (response-template "O") ; err-invalid-move
-             (set-update-time game-data client-id game-name))))
-
-(define (op-create game-data client-id game-name game-pwd response-template)
-  (println "creating a game...")
-  (println game-data)
-  (println game-name)
-  (if (not (hash-has-key? game-data game-name))
-      (if (< (hash-count game-data) 5)
-          (let* ([controller "controller"] ;FIX
-                 [game-pieces "VGSL..."]
-                 [piece-player "121212..."])
-            (response-template (string-join (list "F" client-id game-name game-pwd game-pieces piece-player) ":")) ; op-game-created
-            (hash-set game-data game-name (hash "controller" controller
-                                                     "last-move" (list 99 99)
-                                                     "pwd" game-pwd
-                                                     "1-connected" #t
-                                                     "2-connected" null
-                                                     "1-last-updated" (current-seconds)
-                                                     "2-last-updated" null)))
-          (begin (response-template "T") ; err-too-many-games
-                 game-data))
-      (begin (response-template "Q") ; err-invalid-name
+        [(regexp-match-exact? #px"[M | O | P]" code) (destroy-game game-data name)]) ; FIX try putting this in a match, with this as an else
+      (begin (resp-msg "P") ; err-wrong-credentials
              game-data)))
 
-(define (op-join game-data client-id game-name game-pwd response-template)
-  (if (null? (hash-ref (hash-ref game-data game-name) "2-connected"))
-      (begin (response-template (string-join (list "H" client-id game-name game-pwd ",") ":")) ; op-forward-join
-             (hash-set game-data game-name (hash-set* (hash-ref game-data game-name) "2-connected" #t
-                                                                                     "2-last-updated" (current-seconds))))
-      (begin (response-template "R") ; err-3s-a-crowd
-             game-data)))
-
-(define (op-leave game-data client-id game-name game-pwd response-template)
-  (response-template (string-join (list "J" client-id game-name game-pwd ",") ":")) ; op-forward-leave
-  (hash-set game-data game-name (hash-set* (hash-ref game-data game-name) (string-join (list client-id "-connected") "") #f 
-                                                                          (string-join (list client-id "-last-updated") "") null)))
-
-(define (op-joined? game-data client-id game-name game-pwd response-template)
-  (define is-2-connected? (hash-ref (hash-ref game-data game-name) "2-connected"))
+(define (match-opcode game-data client-msg resp-msg)
   (cond
-    [(null? is-2-connected?) (response-template (string-join (list "A" client-id game-name game-pwd ",") ":"))] ; op-keepalive
-    [(equal? #t is-2-connected?) (response-template (string-join (list "H" client-id game-name game-pwd ",") ":"))] ; op-forward-join
-    [(not is-2-connected?) (response-template (string-join (list "J" client-id game-name game-pwd ",") ":"))]) ; op-forward-leave
-  (set-update-time game-data client-id game-name))
+    [(or (string-prefix? client-msg "B") (string-prefix? client-msg "C")) 
+     (match (regexp-split #px":" client-msg) [(list code id name pwd msg1 msg2) (exec-opcode game-data code id name pwd msg1 msg2 null resp-msg)])]
+    [(string-prefix? client-msg "E") (match (regexp-split #px":" client-msg) [(list code id name pwd msg1 msg2 msg3) (exec-opcode game-data code id name pwd msg1 msg2 msg3 resp-msg)])]
+    [else (match (regexp-split #px":" client-msg) [(list code id name pwd) (exec-opcode game-data code id name pwd null null null resp-msg)])]))
 
 (define (correct-structure client-msg)
-  ; opcode : client-id : game-name : game-password : message
+  ; opcode : id : name : game-password : message
   (println "Checking structure...")
-  (let [(regex-match (or (regexp-match-exact? #px"[A | I | M | O | P]:[1 | 2]:([A-Za-z0-9]){1,20}:([A-Za-z0-9]){1,20}:," client-msg) 
-                         (regexp-match-exact? #px"[B | C]:[1 | 2]:([A-Za-z0-9]){1,20}:([A-Za-z0-9]){1,20}:[0-9][0-9]?,[0-9][0-9]?" client-msg)
-                         (regexp-match-exact? #px"[E | K]:1:([A-Za-z0-9]){1,20}:([A-Za-z0-9]){1,20}:," client-msg)
-                         (regexp-match-exact? #px"G:2:([A-Za-z0-9]){1,20}:([A-Za-z0-9]){1,20}:," client-msg)))]
+  (let [(regex-match (or (regexp-match-exact? #px"[A | M | O | P]:[1 | 2]:([A-Za-z0-9]){1,20}:([A-Za-z0-9]){1,20}" client-msg) 
+                         (regexp-match-exact? #px"[B | C]:[1 | 2]:([A-Za-z0-9]){1,20}:([A-Za-z0-9]){1,20}:[0-8][0-4]:[0-8][0-4]" client-msg)
+                         (regexp-match-exact? #px"E:1:([A-Za-z0-9]){1,20}:([A-Za-z0-9]){1,20}:[L | V | Z | G | S | W | P]{32}:[1 | 2]{32}:[1 | 2]" client-msg)
+                         (regexp-match-exact? #px"K:1:([A-Za-z0-9]){1,20}:([A-Za-z0-9]){1,20}" client-msg)
+                         (regexp-match-exact? #px"G:2:([A-Za-z0-9]){1,20}:([A-Za-z0-9]){1,20}" client-msg)))]
     (println regex-match)
     regex-match))
 
@@ -137,36 +137,19 @@
               (remove-stale-games (destroy-game game-data (car name-list)) (cdr name-list))
               (remove-stale-games game-data (cdr name-list)))))))
 
-(define (find-opcode game-data code client-id game-name game-pwd msg response-template)
-  (println "structure correct!")
-  (let ([moves (map string->number (regexp-split #px"," msg))])
-    (if (or (equal? code "E") (credentials-ok? game-data game-name game-pwd)) ; if game is being created, credentials won't be stored yet
-        (cond
-          [(equal? code "A") (op-keepalive game-data client-id game-name game-pwd response-template)]
-          [(equal? code "B") (op-update? game-data client-id game-name game-pwd response-template (first moves) (last moves))] 
-          [(equal? code "C") (op-update game-data client-id game-name game-pwd response-template (first moves) (last moves))] 
-          [(equal? code "E") (op-create game-data client-id game-name game-pwd response-template)] 
-          [(equal? code "G") (op-join game-data client-id game-name game-pwd response-template)] 
-          [(equal? code "I") (op-leave game-data client-id game-name game-pwd response-template)] 
-          [(equal? code "K") (op-joined? game-data client-id game-name game-pwd response-template)] 
-  
-          [(regexp-match-exact? #px"[M | O | P]" code) (destroy-game game-data game-name)]) ; FIX try putting this in a match, with this as an else
-        (begin (response-template "P") ; err-wrong-credentials
-               game-data))))
-
 ; returns updated game data
 (define (check-message channel-msg game-data)
   (if (equal? channel-msg "Garbage day!")
       (remove-stale-games game-data (hash-keys game-data))
       (let ([client-msg (hash-ref channel-msg "client-msg")]
-            [response-template (hash-ref channel-msg "response-template")])
+            [resp-msg (hash-ref channel-msg "response")])
         (println "checkmsg")
         (println channel-msg)
         (println (hash-ref channel-msg "client-msg"))
         (println (regexp-split #px":" client-msg))
         (if (correct-structure client-msg)
-            (match (regexp-split #px":" client-msg) [(list code client-id game-name game-pwd msg) (find-opcode game-data code client-id game-name game-pwd msg response-template)])
-            (begin (response-template "M") ; err-invalid-code
+            (match-opcode game-data client-msg resp-msg)
+            (begin (resp-msg "M") ; err-invalid-code
                    game-data)))))
 
 (define (history)
@@ -185,7 +168,7 @@
     (display msg out)
     (close-input-port in)
     (close-output-port out))
-  (channel-put conn-channel (hash "client-msg" (read-line in) "response-template" response)))
+  (channel-put conn-channel (hash "client-msg" (read-line in) "response" response)))
 
 ; GARGABE COLLECTION
 
